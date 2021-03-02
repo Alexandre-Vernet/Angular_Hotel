aee15fac44640db84fb64b0f83cec3c55dec1165
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-03-02 16:09:34 +0100

    Add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReceptionComponent } from './reception/reception.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { RoomComponent } from './room/room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ReceptionComponent,
+  },
+  {
+    path: 'restaurant',
+    component: RestaurantComponent,
+  },
+  {
+    path: 'room',
+    component: RoomComponent,
+  },
+  {
+    path: '**',
+    component: ReceptionComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
