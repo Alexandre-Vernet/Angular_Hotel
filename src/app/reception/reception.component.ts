082cd3f35d2fc13fcc6cb0cf1e63b67150fdcf12
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-03-02 16:19:20 +0100

    Bug fixed

--- src/app/reception/reception.component.ts
+++ src/app/reception/reception.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reception',
   templateUrl: './reception.component.html',
-  styleUrls: ['./reception.component.scss']
+  styleUrls: ['./reception.component.scss'],
 })
 export class ReceptionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
